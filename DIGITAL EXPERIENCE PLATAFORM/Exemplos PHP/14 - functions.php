baa42963dfecc97e9e2facf09b0b61dc70f63c57
Joel Silva <joelcsilva@outlook.com>
2017-02-13 20:45:34 -0200

    arquivos aula 13022017

---
+++ DIGITAL EXPERIENCE PLATAFORM/Exemplos PHP/14 - functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    
+        function digaAlgo(){
+            echo "Olá !! <br>";
+        }
+
+        digaAlgo();
+
+
+        function calcula(){
+            echo 452 * 7 + 2;
+        };
+
+        calcula();
+
+    ?>
+</body>
+</html>
