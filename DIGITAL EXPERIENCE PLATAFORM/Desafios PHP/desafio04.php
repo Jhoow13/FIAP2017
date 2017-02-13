1f0f5246a083d4bdbb2fbadb1f9fc14542c5b3ba
Joel Silva <joelcsilva@outlook.com>
2017-02-13 20:07:38 -0200

    Resolução parcial desafio04 - 13022017

--- DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
+++ DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
@@ -2,12 +2,18 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>PHP</title>
+    <title>Tabuada - Desafio 04</title>
 </head>
 <body>
     <?php
     
-        
+        $valores = array(1,2,3,4,5,6,7,8,9,10);    
+
+        foreach ($valores as $valor){
+            for($i = 0; $i <= 10; $i++){
+                echo $valor * $i . '<br>';
+            }
+        }
 
     ?>
 </body>
