45c5d2514431f9246f1589b3ca061e4bdacd9dea
Joel Silva <joelcsilva@outlook.com>
2017-02-13 20:16:28 -0200

    Desafio04 final - 13022017

--- DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
+++ DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
@@ -10,9 +10,11 @@
         $valores = array(1,2,3,4,5,6,7,8,9,10);    
 
         foreach ($valores as $valor){
+            echo "Tabuada do " . $valor . '<br>';
             for($i = 0; $i <= 10; $i++){
-                echo $valor * $i . '<br>';
+                echo "$valor x $i = " . $valor * $i . '<br>';
             }
+            echo "____________ <br>";
         }
 
     ?>
