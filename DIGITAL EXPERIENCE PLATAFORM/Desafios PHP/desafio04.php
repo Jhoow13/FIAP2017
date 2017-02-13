419429b0fadad026d05676f3dc265f74d706ba28
Joel Silva <joelcsilva@outlook.com>
2017-02-13 20:39:32 -0200

    Arquivo desafio05 e solucao prof desafio04 - 13022017

--- DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
+++ DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio04.php
@@ -6,9 +6,15 @@
 </head>
 <body>
     <?php
-    
-        $valores = array(1,2,3,4,5,6,7,8,9,10);    
+        //Solução professor
+        // for($x = 1; $x<=10; $x++){
+        //     echo "Tabuada do $x <br>";
+        //     for($y = 1; $y<=10; $y++){
+        //         echo "$x x $y = " . $x * $y . '<br>';
+        //     }
+        // }
 
+        $valores = array(1,2,3,4,5,6,7,8,9,10);
         foreach ($valores as $valor){
             echo "Tabuada do " . $valor . '<br>';
             for($i = 0; $i <= 10; $i++){
