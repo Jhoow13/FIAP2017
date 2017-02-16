aacd031c9e63a26be2d51040837f38d749619e90
Joel Silva <joelcsilva@outlook.com>
2017-02-16 01:44:39 -0200

    Criando logica inicial calculadora

--- DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
+++ DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
@@ -2,15 +2,29 @@ package br.com.fiap.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculadoraActivity extends AppCompatActivity {
 
-    EditText numero1;
+    EditText numero1, numero2;
+    TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+
+        numero1 = (EditText) findViewById(R.id.tvNumero1);
+        numero2 = (EditText) findViewById(R.id.tvNumero2);
+        tvResultado = (TextView) findViewById(R.id.tvResultado);
+
+    }
+
+    public void realizarConta(View v){
+        int resultado = Integer.parseInt(numero1.getText().toString()) + Integer.parseInt(numero2.getText().toString());
+
+        tvResultado.setText(String.valueOf(resultado));
     }
 }
