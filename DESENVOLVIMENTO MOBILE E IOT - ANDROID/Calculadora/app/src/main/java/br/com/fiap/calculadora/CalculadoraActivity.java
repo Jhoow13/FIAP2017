3dcd4aba504b1edc4fc4ac4b658f729aa2e42f6c
Joel Silva <joelcsilva@outlook.com>
2017-02-16 17:48:52 -0200

    Implementando logica OnClickListener

--- DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
+++ DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
@@ -3,70 +3,55 @@ package br.com.fiap.calculadora;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class CalculadoraActivity extends AppCompatActivity {
+public class CalculadoraActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText numero1, numero2;
     TextView tvResultado;
     String operacao;
+    Button btnSoma, btnSubtracao, btnMultiplicacao, btnDivisao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
-
         numero1 = (EditText) findViewById(R.id.tvNumero1);
         numero2 = (EditText) findViewById(R.id.tvNumero2);
         tvResultado = (TextView) findViewById(R.id.tvResultado);
+
+        btnSoma = (Button) findViewById(R.id.btnSoma);
+        btnSoma.setOnClickListener(this);
+
+        btnSubtracao = (Button) findViewById(R.id.btnSubtracao);
+        btnSubtracao.setOnClickListener(this);
+
+        btnMultiplicacao = (Button) findViewById(R.id.btnMultiplicacao);
+        btnMultiplicacao.setOnClickListener(this);
+
+        btnDivisao = (Button) findViewById(R.id.btnDivisao);
+        btnDivisao.setOnClickListener(this);
     }
 
-
-    public String defineOperacao(View v){
-
-        switch (v.getId()) {
-            case R.id.btnSoma:
-                operacao = "soma";
-                break;
-            case R.id.btnSubtracao:
-                operacao = "subtracao";
-                break;
-            case R.id.btnMultiplicacao:
-                operacao = "multiplicacao";
-                break;
-            case R.id. btnDivisao:
-                operacao = "divisao";
-                break;
-            default:
-                operacao = "";
-                break;
+    public void onClick(View v){
+        int resultado;
+        if (v == btnSoma){
+            resultado = Integer.parseInt(numero1.getText().toString()) + Integer.parseInt(numero2.getText().toString());
+            tvResultado.setText(String.valueOf(resultado));
         }
-
-        return operacao;
-    }
-
-    public void realizarConta(View v){
-
-        int resultado = 0;
-
-        switch (operacao){
-            case "soma":
-                int soma = Integer.parseInt(numero1.getText().toString()) + Integer.parseInt(numero2.getText().toString());
-                tvResultado.setText(String.valueOf(soma));
-                break;
-            case "subtracao":
-                int subtracao = Integer.parseInt(numero1.getText().toString()) - Integer.parseInt(numero2.getText().toString());
-                tvResultado.setText(String.valueOf(subtracao));
-                break;
-            case "multiplicacao":
-                int multiplicacao = Integer.parseInt(numero1.getText().toString()) * Integer.parseInt(numero2.getText().toString());
-                break;
-            case "divisao":
-                int divisao = Integer.parseInt(numero1.getText().toString()) / Integer.parseInt(numero2.getText().toString());
-                break;
+        else if(v == btnSubtracao){
+            resultado = Integer.parseInt(numero1.getText().toString()) - Integer.parseInt(numero2.getText().toString());
+            tvResultado.setText(String.valueOf(resultado));
         }
-
-
+        else if(v == btnMultiplicacao){
+            resultado = Integer.parseInt(numero1.getText().toString()) * Integer.parseInt(numero2.getText().toString());
+            tvResultado.setText(String.valueOf(resultado));
+        }
+        else if(v == btnDivisao){
+            resultado = Integer.parseInt(numero1.getText().toString()) / Integer.parseInt(numero2.getText().toString());
+            tvResultado.setText(String.valueOf(resultado));
+        }
     }
 }
