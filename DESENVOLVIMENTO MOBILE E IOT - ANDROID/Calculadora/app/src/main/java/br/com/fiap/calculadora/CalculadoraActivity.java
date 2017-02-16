eb46a59aa78dc95929dca580ed814b16c9f4ca01
Joel Silva <joelcsilva@outlook.com>
2017-02-16 17:38:40 -0200

    Logica botao soma e resultado

--- DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
+++ DESENVOLVIMENTO MOBILE E IOT - ANDROID/Calculadora/app/src/main/java/br/com/fiap/calculadora/CalculadoraActivity.java
@@ -10,6 +10,7 @@ public class CalculadoraActivity extends AppCompatActivity {
 
     EditText numero1, numero2;
     TextView tvResultado;
+    String operacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +20,53 @@ public class CalculadoraActivity extends AppCompatActivity {
         numero1 = (EditText) findViewById(R.id.tvNumero1);
         numero2 = (EditText) findViewById(R.id.tvNumero2);
         tvResultado = (TextView) findViewById(R.id.tvResultado);
+    }
 
+
+    public String defineOperacao(View v){
+
+        switch (v.getId()) {
+            case R.id.btnSoma:
+                operacao = "soma";
+                break;
+            case R.id.btnSubtracao:
+                operacao = "subtracao";
+                break;
+            case R.id.btnMultiplicacao:
+                operacao = "multiplicacao";
+                break;
+            case R.id. btnDivisao:
+                operacao = "divisao";
+                break;
+            default:
+                operacao = "";
+                break;
+        }
+
+        return operacao;
     }
 
     public void realizarConta(View v){
-        int resultado = Integer.parseInt(numero1.getText().toString()) + Integer.parseInt(numero2.getText().toString());
 
-        tvResultado.setText(String.valueOf(resultado));
+        int resultado = 0;
+
+        switch (operacao){
+            case "soma":
+                int soma = Integer.parseInt(numero1.getText().toString()) + Integer.parseInt(numero2.getText().toString());
+                tvResultado.setText(String.valueOf(soma));
+                break;
+            case "subtracao":
+                int subtracao = Integer.parseInt(numero1.getText().toString()) - Integer.parseInt(numero2.getText().toString());
+                tvResultado.setText(String.valueOf(subtracao));
+                break;
+            case "multiplicacao":
+                int multiplicacao = Integer.parseInt(numero1.getText().toString()) * Integer.parseInt(numero2.getText().toString());
+                break;
+            case "divisao":
+                int divisao = Integer.parseInt(numero1.getText().toString()) / Integer.parseInt(numero2.getText().toString());
+                break;
+        }
+
+
     }
 }
