661c7312e0134fd5788cb5ad9f93acd8d5d49ded
Joel Silva <joelcsilva@outlook.com>
2017-03-31 20:10:11 -0300

    Desafio10 finalizado

--- DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio10/desafio10.php
+++ DIGITAL EXPERIENCE PLATAFORM/Desafios PHP/desafio10/desafio10.php
@@ -5,6 +5,15 @@ Usando o método GET, crie um formulário que receba o nome do estudante,
 Faça a validação dos campos de modo que usuário tenha que preencher todos os campos.
 */
 
+    if(isset($_GET['btnCalcular'])){
+
+        $nome = $_GET['txtNome'];
+        $nota1 = $_GET['txtNota1'];
+        $nota2 = $_GET['txtNota2'];
+
+        $media = ($nota1 + $nota2) / 2;        
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -18,27 +27,48 @@ Faça a validação dos campos de modo que usuário tenha que preencher todos os
 
   <div class="container">
 
+      <h1 class="text-center">DESAFIO 10 - NOTAS</h1>
       <div class="col-sm-6">
-
-        <h1 class="text-center">DESAFIO 10 - NOTAS</h1>
         <form action="desafio10.php" method="GET">
           <div class="form-group">
             <label for="">Nome</label>
-            <input type="text" class="form-control">
+            <input type="text" name="txtNome" class="form-control">
           </div>
           <div class="form-group">
             <label for="">NAC 1</label>
-            <input type="text" class="form-control">
+            <input type="text" name="txtNota1" class="form-control">
           </div>
           <div class="form-group">
             <label for="">NAC 2</label>
-            <input type="text" class="form-control">
+            <input type="text" name="txtNota2" class="form-control">
           </div>
 
-          <input type="submit" value="Calcular Media" class="form-control btn btn-primary">
+          <input type="submit" name="btnCalcular" value="Calcular Media" class="form-control btn btn-primary">
 
         </form>
 
+    </div>
+
+    <div class="col-sm-6">
+        <p class="text-center">Resumo</p>
+        <table class="table table-bordered">
+          <tr>
+            <td>Nome</td>
+            <td><?php if(isset($nome)){ echo $nome; }?></td>
+          </tr>
+          <tr>
+            <td>Nota 1</td>
+            <td><?php if(isset($nota1)){ echo $nota1; }?></td>                        
+          </tr>
+          <tr>
+            <td>Nota 2</td>
+            <td><?php if(isset($nota2)){ echo $nota2; }?></td>
+          </tr>
+          <tr>
+            <td>Media</td>
+            <td><?php if(isset($media)){ echo $media; }?></td>
+          </tr>
+        </table>
     </div>
   </div>
 
